d80e4e8113dbca5f52db968e90fa12e47cc56d4d
sozi <sozyarnadir@gmail.com>
2022-10-24 17:06:48 +0300

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+const API_URL = "https://randomfox.ca/floof/"
+
+
+function App() {
+  const [image, setImage] = useState(null)
+
+  useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(data => setImage(data.message))
+  }, [])
+
+  if (!image) {
+      return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App
